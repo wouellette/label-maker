578e17a1b55c948d78846856ad08c26f22e8a9cd
William Ouellette <william.ouellette@gmail.com>
2019-02-02 16:35:12 +0100

    add vector format case for match_class

--- label_maker/utils.py
+++ label_maker/utils.py
@@ -14,14 +14,17 @@ def url(tile, imagery):
     """Return a tile url provided an imagery template and a tile"""
     return imagery.replace('{x}', tile[0]).replace('{y}', tile[1]).replace('{z}', tile[2])
 
-def class_match(ml_type, label, i):
+def class_match(ml_type, label, format, i):
     """Determine if a label matches a given class index"""
     if ml_type == 'classification':
         return label[i] > 0
     elif ml_type == 'object-detection':
         return len(list(filter(lambda bb: bb[4] == i, label)))
     elif ml_type == 'segmentation':
-        return np.count_nonzero(label == i)
+        if format == 'vector':
+            return label[1] == i
+        else:    
+            return np.count_nonzero(label == i)
     return None
 
 def download_tile_tms(tile, imagery, folder, *args):
